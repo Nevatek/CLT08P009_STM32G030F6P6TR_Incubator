67cb8e370355452b1e7f5d665d1c39c0f21441e1
Aldrin Rebellow <aldrin.rebellow@nestgroup.net>
2026-04-01 22:10:02 +0530

    Initial comit

---
+++ Core/Inc/main.h
@@ -0,0 +1,93 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32g0xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+void HAL_TIM_MspPostInit(TIM_HandleTypeDef *htim);
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define NC_Pin GPIO_PIN_9
+#define NC_GPIO_Port GPIOB
+#define NCC15_Pin GPIO_PIN_15
+#define NCC15_GPIO_Port GPIOC
+#define NTC_ADC_0_Pin GPIO_PIN_0
+#define NTC_ADC_0_GPIO_Port GPIOA
+#define NTC_ADC_1_Pin GPIO_PIN_1
+#define NTC_ADC_1_GPIO_Port GPIOA
+#define TC_Pin GPIO_PIN_4
+#define TC_GPIO_Port GPIOA
+#define PELTIER_DIR0_Pin GPIO_PIN_5
+#define PELTIER_DIR0_GPIO_Port GPIOA
+#define FAN_PWM_Pin GPIO_PIN_6
+#define FAN_PWM_GPIO_Port GPIOA
+#define PELTIER_DIR1_Pin GPIO_PIN_7
+#define PELTIER_DIR1_GPIO_Port GPIOA
+#define PELTIER_PWM1_Pin GPIO_PIN_0
+#define PELTIER_PWM1_GPIO_Port GPIOB
+#define PELTIER_PWM0_Pin GPIO_PIN_11
+#define PELTIER_PWM0_GPIO_Port GPIOA
+#define NCA12_Pin GPIO_PIN_12
+#define NCA12_GPIO_Port GPIOA
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
